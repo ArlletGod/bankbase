be6ee66f5ea03150a66c4ce3a53b14564a131e6e
Maxim Sucicov <arlletgod@gmail.com>
2020-05-26 22:50:17 +0300

    initial comit of the project

---
+++ src/main/java/com/myex/bankbase/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.myex.bankbase.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller
+public class MainController {
+
+@GetMapping("/")
+public String index( Model model) {
+
+    return"index";
+}
+
+}
